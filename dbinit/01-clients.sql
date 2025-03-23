b6ff694768a428d58306df31279d6c4ebee4f5b9
Gregorio Alvarez <allgrego14@gmail.com>
2025-03-23 17:08:35 -0400

    [feature] More data added to clients and providers

--- dbinit/01-clients.sql
+++ dbinit/01-clients.sql
@@ -7,5 +7,10 @@ SET search_path TO clients, public;
 -- Create the clients table
 CREATE TABLE IF NOT EXISTS clients (
     client_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(255) UNIQUE NOT NULL -- Or TEXT for potentially longer names
+    name VARCHAR(255) UNIQUE NOT NULL, -- Or TEXT for potentially longer names
+    tax_id VARCHAR(100) UNIQUE,
+    contact_name VARCHAR(255),
+    contact_email VARCHAR(255),
+    contact_phone VARCHAR(100),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
